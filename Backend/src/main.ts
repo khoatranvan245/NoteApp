81877b70e56ae26acbe44693a4dd9239e45dff98
Tran Van Khoa <khoatranvan245@gmail.com>
2025-01-06 09:39:35 +0700

    install mongodb

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import mongoose from "mongoose";
 dotenv.config();
 const app = express();
+
+mongoose.connect(process.env.DB_URL!);
 
 app.listen(3333, () => {
   console.log("Hello world");
