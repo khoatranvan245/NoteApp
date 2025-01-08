23299103e52ebb7d45a2cc9be10b09fe7e37a34a
Tran Van Khoa <khoatranvan245@gmail.com>
2025-01-08 22:41:22 +0700

    add login feature

--- Backend/src/authServer.ts
+++ Backend/src/authServer.ts
@@ -1,8 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import authRouter from "./router/authRouter";
+import mongoose from "mongoose";
 
 dotenv.config();
 const app = express();
+mongoose.connect(process.env.DB_URL!);
+app.use(express.json());
+
+app.use("/api/v1", authRouter);
 
 app.listen(4444, () => {
   console.log("auth running...");
