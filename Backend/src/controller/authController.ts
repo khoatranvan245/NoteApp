87acdbe66268f8822102b97105b7a9860051428c
Tran Van Khoa <khoatranvan245@gmail.com>
2025-01-10 10:35:20 +0700

    add refresh token feature

--- Backend/src/controller/authController.ts
+++ Backend/src/controller/authController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import jwt, { Secret } from "jsonwebtoken";
 import User from "../model/user";
+import tokenType from "../type/tokenType";
 
 const login = async (req: Request, res: Response): Promise<any> => {
   const { username, password } = req.body;
@@ -14,8 +15,32 @@ const login = async (req: Request, res: Response): Promise<any> => {
   const token = jwt.sign({ user }, process.env.ACCESS_TOKEN as Secret, {
     expiresIn: "30m",
   });
+  const refreshToken = jwt.sign({ user }, process.env.REFRESH_TOKEN as Secret, {
+    expiresIn: "30d",
+  });
 
-  return res.status(200).json({ token });
+  return res.status(200).json({ token, refreshToken });
 };
 
-export { login };
+const refreshToken = async (req: Request, res: Response): Promise<any> => {
+  const { refreshToken } = req.body;
+
+  try {
+    const decode = jwt.verify(
+      refreshToken,
+      process.env.REFRESH_TOKEN as Secret,
+    ) as tokenType;
+
+    const token = jwt.sign(
+      { user: decode.user._id },
+      process.env.ACCESS_TOKEN as Secret,
+      { expiresIn: "30m" },
+    );
+
+    return res.status(200).json({ token });
+  } catch (err) {
+    return res.status(401).json({ error: "Refresh token invalid!" });
+  }
+};
+
+export { login, refreshToken };
--- Backend/src/router/authRouter.ts
+++ Backend/src/router/authRouter.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { login } from "../controller/authController";
+import { login, refreshToken } from "../controller/authController";
 
 const router = express.Router();
 
 router.get("/login", login);
+router.get("/refresh", refreshToken);
 
 export default router;
