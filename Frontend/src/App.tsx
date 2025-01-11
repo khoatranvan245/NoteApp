e75a443baa3c060d71c1fb42aa8683fd5c641cbf
Tran Van Khoa <khoatranvan245@gmail.com>
2025-01-12 00:03:50 +0700

    change sign up page design

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { FormEvent, useState } from "react"
-
+import styles from "./app.module.scss"
 function App() {
   const [username, setUsername] = useState<string>()
   const [password, setPassword] = useState<string>()
@@ -14,16 +14,23 @@ function App() {
       body: JSON.stringify({ username, password })
     })
     const data = await response.json()
-    console.log(data)
+    console.log(data.token)
   }
 
-  return <form onSubmit={(e) => handleSubmit(e)}>
-    <p>Username</p>
-    <input type="text" name="username" onChange={(e) => { setUsername(e.target.value) }} />
-    <p>Password</p>
-    <input type="password" name="password" onChange={(e) => { setPassword(e.target.value) }} />
-    <input type="submit" />
-  </form>
+  return (
+    <div className={styles.container}>
+      <div className={styles.formContainer}>
+        <p className={styles.title}>Sign in to your account</p>
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <p>Username</p>
+          <input type="text" name="username" onChange={(e) => { setUsername(e.target.value) }} />
+          <p>Password</p>
+          <input type="password" name="password" onChange={(e) => { setPassword(e.target.value) }} />
+          <input type="submit" value="Sign in" />
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default App
